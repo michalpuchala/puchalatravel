3084fab56398d2c0cab5913e65a4508b84b1e971
Michal Puchala <m.puchala@criteo.com>
2018-09-04 18:58:14 +0200

    adding the data model

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Category, Tag, Author, Trip, Image, Post
 
 admin.site.register(Post)
+admin.site.register(Image)
+admin.site.register(Trip)
+admin.site.register(Tag)
+admin.site.register(Category)
+admin.site.register(Author)
--- blog/models.py
+++ blog/models.py
@@ -2,12 +2,70 @@ from django.db import models
 from django.utils import timezone
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=32)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __unicode__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=32)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    bio = models.CharField(max_length=5000)
+
+    def __str__(self):
+        return self.first_name
+
+
+class Trip(models.Model):
+    name = models.CharField(max_length=32)
+    destination = models.CharField(max_length=50)
+    start_date = models.DateTimeField(blank=True, null=True)
+    end_date = models.DateTimeField(blank=True, null=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Image(models.Model):
+    name = models.CharField(max_length=32)
+    picture = models.ImageField(default='defualt.png')
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
+    taken = models.DateTimeField(blank=True, null=True)
+    uploaded = models.DateTimeField(default=timezone.now)
+    width = models.IntegerField()
+    height = models.IntegerField()
+
+    def __unicode__(self):
+        return self.name
+
+
 class Post(models.Model):
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    category = models.ManyToManyField(Category)
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, unique=True)
+    byline = models.CharField(max_length=255)
+    tag = models.ManyToManyField(Tag)
     text = models.TextField()
+    slug = models.SlugField(max_length=128)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    main_image = models.ForeignKey(Image, on_delete=models.CASCADE, related_name='main_image')
+    images = models.ManyToManyField(Image, related_name='images')
+    instagram_link = models.URLField()
+    twitter_link = models.URLField()
 
     def publish(self):
         self.published_date = timezone.now()
