967cd855d6ade5da8995e10a2db37daa346b0a05
michalpuchala <mk.puchala@gmail.com>
2018-09-06 20:09:12 +0200

    pierwsza wersja strony glownej

--- blog/views.py
+++ blog/views.py
@@ -5,4 +5,10 @@ from .models import Post
 
 def index(request):
     posts = Post.objects.all().order_by('published_date')
-    return render(request, 'blog/index.html', {'posts': posts})
+    latest_post = Post.objects.all().order_by('published_date')[0]
+    second_latest_post = Post.objects.all().order_by('published_date')[1]
+    third_latest_post = Post.objects.all().order_by('published_date')[2]
+    return render(request, 'blog/index.html', {'posts': posts,
+                                                'latest_post': latest_post,
+                                                'second_latest_post': second_latest_post,
+                                                'third_latest_post': third_latest_post})
