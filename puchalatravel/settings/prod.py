be84e2f543b88e4ea3f02b7720a513756bfdf1dc
m.puchala <m.puchala@criteo.com>
2019-02-10 22:04:03 +0100

    fixing settings

--- puchalatravel/settings/prod.py
+++ puchalatravel/settings/prod.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'puchalatravel.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../../db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../db.sqlite3'),
     }
 }
 
