837aeb50af80334780367a8a9f1aeef6bd340260
m.puchala <m.puchala@criteo.com>
2019-02-10 21:48:59 +0100

    adding prod and dev settings

--- puchalatravel/settings/dev.py
+++ puchalatravel/settings/dev.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'puchalatravel.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../../db.sqlite3'),
     }
 }
 
@@ -126,9 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '../../static')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '../../media')
 
 # Markdownx settings
 
