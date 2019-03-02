91ad8075a1785429f06a500e3487df596d8a16b6
m.puchala <m.puchala@criteo.com>
2019-03-02 20:26:10 +0100

    hotfix

--- puchalatravel/settings/prod.py
+++ puchalatravel/settings/prod.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print("base dir path", BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -137,9 +138,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '../../static')
+STATIC_ROOT = os.path.join(BASE_DIR, '../static')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '../../media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '../media')
 
 # Markdownx settings
 
