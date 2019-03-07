bf1f149e995f7e8553a9192f2f96089ee009d4a4
m.puchala <m.puchala@criteo.com>
2019-03-07 23:18:35 +0100

    fixing map template

--- puchalatravel/settings/prod.py
+++ puchalatravel/settings/prod.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'puchalatravel.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, '../../blog/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
