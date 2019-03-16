3ec75b0b5428419adbbf022f8db78499e65c6920
m.puchala <m.puchala@criteo.com>
2019-03-16 20:13:09 +0100

    revert password in settings

--- puchalatravel/settings/prod.py
+++ puchalatravel/settings/prod.py
@@ -155,8 +155,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "puchalatravel@gmail.com" #os.environ.get('EMAIL_USR')
-EMAIL_HOST_PASSWORD = "cspghkcvxerjjrva" #os.environ.get('EMAIL_PWD')
+EMAIL_HOST_USER = os.environ.get('EMAIL_USR')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PWD')
 
 # crispy forms
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
