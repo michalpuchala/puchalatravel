508e71b582d3edbc1adca61b3d69f64ccd9b7f50
m.puchala <m.puchala@criteo.com>
2019-03-16 20:36:41 +0100

    fixing email

--- puchalatravel/settings/prod.py
+++ puchalatravel/settings/prod.py
@@ -155,7 +155,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_USR')
+EMAIL_HOST_USER = "puchalatravel@gmail.com"
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PWD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
