fb80e877128538f72162050e21b5d54dffaee5f8
Ubuntu <ubuntu@ip-172-31-35-104.us-east-2.compute.internal>
2019-04-23 15:10:22 +0000

    adding dotenv to wsgi

--- puchalatravel/wsgi.py
+++ puchalatravel/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
-
+from dotenv import load_dotenv
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'puchalatravel.settings.prod')
-
+project_folder = os.path.expanduser('/home/ubuntu/puchalatravel/')
+load_dotenv(os.path.join(project_folder, '.env'))
 application = get_wsgi_application()
